0006834a448d7c981a5cb0cc169ffad48db777af
Guinsly Mondesir <guinslym@gmail.com>
2018-09-14 02:06:37 +0000

    changing db to postgres

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'djangodocker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql', 
@@ -80,8 +80,18 @@ DATABASES = {
     'USER': 'root',
     'PASSWORD': 'root',
     'HOST': 'db', # Or an IP Address that your DB is hosted on
-    'PORT': '3306',
+    'PORT': '3406',
   }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
